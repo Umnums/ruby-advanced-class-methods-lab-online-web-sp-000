f40f930b1e441784d6df386c0b4c71c2031fc50b
Joseph Kao <josephtkao@gmail.com>
2020-04-14 19:43:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,7 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) == name
       return self.find_by_name(name)
-    else
-      return self.create_by_name(name)
+
     end
   end
 
