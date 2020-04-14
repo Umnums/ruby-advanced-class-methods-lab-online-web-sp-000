2563d832712d264624ef26f8241f9ba113200e4f
Joseph Kao <josephtkao@gmail.com>
2020-04-14 19:42:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,42 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    new_song = Song.new
+    self.all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    self.all << new_song
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.each do |songs|
+      if songs.name == name
+        return songs
+      end
+    end
+    return nil
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == name
+      return
+    else
+      return self.create_by_name(name)
+    end
+  end
+
   def self.all
     @@all
   end
